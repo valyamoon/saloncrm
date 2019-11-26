197908c2a24d4081e009e0e74a630442ae96ec61
Nadim Shah <nadim.shah@smartdatainc.net>
2019-11-26 13:41:45 +0530

    Initial commit

--- api/controller/auth/auth_router.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function(router) {
-    const auth = require('./auth_ctrl');
-    router.get('/api/howdy/login', auth.userLogin);
-    router.get('/api/howdy/test', auth.testSwaggerRequest);
-}
--- api/router.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(express) {
-    const router = express;
-    require('./controller/auth/auth_router')(router);
-    require('./controller/product/product_router')(router);
-    return router;
-}
