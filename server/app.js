2c32bde31b5c64ae15de9a19807b76f925e3670a
Nadim Shah <nadim.shah@smartdatainc.net>
2019-11-29 13:17:29 +0530

    REsolved issue for staging

--- server/app.js
+++ server/app.js
@@ -1,48 +1,3 @@
-// const app = require("express")();
-// const express = require("express");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const logger = require("morgan");
-// const swaggerJSDoc = require("swagger-jsdoc");
-// const swaggerUI = require("swagger-ui-express");
-// config = require("./config/config");
-// const swaggerDefinition = require("./router/swagger");
-// const passport = require("passport");
-
-// // process.env.NODE_ENV = process.env.NODE_ENV || "local"; //local server
-// process.env.NODE_ENV = process.env.NODE_ENV || "staging"; //staging server
-// // process.env.NODE_ENV = process.env.NODE_ENV || 'live'; //live server (www.mdate.app)
-// //dist
-// const config = require("./app/config/config.js").get(process.env.NODE_ENV);
-// const API_PORT = 5977;
-// // const API_PORT = process.env.API_PORT || 3000;
-
-// //5976
-// const options = {
-//   swaggerDefinition,
-//   apis: ["./api/router/*.js"]
-// };
-
-// const swaggerSpec = swaggerJSDoc(options);
-// app.use("/swagger.json", function(req, res) {
-//   res.setHeader("Content-Type", "applicaton/json");
-//   res.send(swaggerSpec);
-// });
-
-// require("./config/db");
-// app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(logger("dev"));
-// app.use(passport.initialize());
-// require("./api/router")(app);
-// app.use(express.static("./dist"));
-
-// app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
-
-// module.exports = app;
-
 const app = require("express")();
 const express = require("express");
 const cors = require("cors");
@@ -74,7 +29,7 @@ app.use("/swagger.json", function(req, res) {
   res.send(swaggerSpec);
 });
 
-require("./config/config");
+require("./config/db");
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -85,8 +40,8 @@ require("./api/router")(app);
 app.use(express.static("./dist"));
 
 var port = process.env.PORT || config.port;
-app.listen(port); //30 min
+// app.listen(port); //30 min
 
-// app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
