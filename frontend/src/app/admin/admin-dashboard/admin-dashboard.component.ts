f5a53ad7b662057f3392a8797af2240c1716f62d
Nadim Shah <nadim.shah@smartdatainc.net>
2020-01-03 14:46:15 +0530

    Admin Panel done

--- frontend/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-
+  isSelectorShown:boolean =true;
   constructor() { }
 
   ngOnInit() {
   }
+  showSelection(data){
+    console.log("HIIIIIIIIIIIIII");
+    this.isSelectorShown =  !this.isSelectorShown;
+
+    data.classList.toggle("change");
+
+  }
+
 
 }
