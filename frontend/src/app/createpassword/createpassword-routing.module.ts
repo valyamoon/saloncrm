e4d4d77ff191d5e7db05fdd4d3323745c8e4fff4
Nadim Shah <nadim.shah@smartdatainc.net>
2020-02-04 15:07:52 +0530

    Added changes

--- frontend/src/app/createpassword/createpassword-routing.module.ts
+++ frontend/src/app/createpassword/createpassword-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CpComponent } from "./cp/cp.component";
 
-const routes: Routes = [{ path: "", component: CpComponent }];
+const routes: Routes = [{ path: "/:id", component: CpComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/createpassword/createpassword.component.ts
+++ frontend/src/app/createpassword/createpassword.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-createpassword',
-  templateUrl: './createpassword.component.html',
-  styleUrls: ['./createpassword.component.css']
+  selector: "app-createpassword",
+  templateUrl: "./createpassword.component.html",
+  styleUrls: ["./createpassword.component.css"]
 })
 export class CreatepasswordComponent implements OnInit {
+  resetKey: any;
+  constructor(private activatedRoutes: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
