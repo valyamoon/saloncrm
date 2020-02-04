daf40848a83b82c7688476904c6141661b4be21b
Nadim Shah <nadim.shah@smartdatainc.net>
2020-02-04 16:35:00 +0530

    Added routes

--- frontend/src/app/createpassword/createpassword-routing.module.ts
+++ frontend/src/app/createpassword/createpassword-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CpComponent } from "./cp/cp.component";
 
-const routes: Routes = [{ path: "", component: CpComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    children: [{ path: "", component: CpComponent }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
