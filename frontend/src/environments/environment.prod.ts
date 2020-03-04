5f1976399e7023b9548718b689471e4f8c189ee2
Nadim Shah <nadim.shah@smartdatainc.net>
2020-03-04 15:26:19 +0530

    added

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   env: "prod",
   production: true,
-  apiUrl: "http://15.236.28.12:5977",
-  baseUrl: "http://15.236.28.12:5977",
-  backendBaseUrl: "http://15.236.28.12:5977"
+  apiUrl: "https://15.236.28.12:5977",
+  baseUrl: "https//15.236.28.12:5977",
+  backendBaseUrl: "https://15.236.28.12:5977"
 };
