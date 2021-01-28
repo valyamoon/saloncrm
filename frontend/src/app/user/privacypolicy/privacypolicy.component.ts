11e252a78479f1a4a2f4579656aeb2be96ab6874
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-28 15:43:07 +0200

    Privacy policy page translation

--- frontend/src/app/user/privacypolicy/privacypolicy.component.ts
+++ frontend/src/app/user/privacypolicy/privacypolicy.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AvailableLanguages } from "../../enums";
+import { LanguagesService } from "../../services";
 
 @Component({
   selector: "app-privacypolicy",
   templateUrl: "./privacypolicy.component.html",
-  styleUrls: ["./privacypolicy.component.scss"]
+  styleUrls: ["./privacypolicy.component.scss"],
 })
-export class PrivacypolicyComponent implements OnInit {
-  constructor() {}
+export class PrivacypolicyComponent implements OnInit, OnDestroy {
+  currentLanguage: AvailableLanguages;
+  currentLanguageSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private languagesService: LanguagesService) {}
+
+  ngOnInit(): void {
+    this.currentLanguageSub = this.languagesService.currentLanguage$.subscribe(
+      (x) => (this.currentLanguage = x)
+    );
+  }
+
+  ngOnDestroy() {
+    this.currentLanguageSub.unsubscribe();
+  }
 }
