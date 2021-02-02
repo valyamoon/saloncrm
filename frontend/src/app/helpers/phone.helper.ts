9405edcf5002e899ea9ba41876668cc8e9bd5d0c
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-02-02 12:41:37 +0200

    get national number and country code

--- frontend/src/app/helpers/phone.helper.ts
+++ frontend/src/app/helpers/phone.helper.ts
@@ -6,3 +6,13 @@ export const getCorrectPhone = (phone: string, countryCode: string): string => {
   const phoneNumber = PNU.parseAndKeepRawInput(phone, countryCode);
   return PNU.format(phoneNumber, PhoneNumberFormat.INTERNATIONAL);
 };
+
+export const getCountryCode = (phone: string) => {
+  const countryCode = PNU.parse(phone).getCountryCode();
+  return countryCode;
+};
+
+export const getNationalNumber = (phone: string) => {
+  const nationalNumber = PNU.parse(phone).getNationalNumber();
+  return nationalNumber;
+};
