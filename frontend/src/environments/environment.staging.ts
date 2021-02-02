c88b9745673d39135d0068fc8bce486d264edb3a
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-02-02 13:11:45 +0200

    changed stage urls

--- frontend/src/environments/environment.staging.ts
+++ frontend/src/environments/environment.staging.ts
@@ -1,9 +1,9 @@
 export const environment = {
   env: "staging",
   production: false,
-  apiUrl: "https://bookapp-staging.herokuapp.com",
-  baseUrl: "https://bookapp-staging.herokuapp.com",
-  backendBaseUrl: "https://bookapp-staging.herokuapp.com",
+  apiUrl: "https://bookapp-ch-stage.herokuapp.com",
+  baseUrl: "https://bookapp-ch-stage.herokuapp.com",
+  backendBaseUrl: "https://bookapp-ch-stage.herokuapp.com",
   translationUrl: "https://translation.googleapis.com/language/translate/v2",
   googleApiKey: "AIzaSyAvJBguyLtqPxDZ7STn25mYxj4yyCH0PYY",
 };
