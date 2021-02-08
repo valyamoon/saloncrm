d8c024aa8dbca1a4255e4d25649c542147c491d1
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-02-08 13:47:07 +0200

    skip english for translate pipe

--- frontend/src/app/pipes/translation.pipe.ts
+++ frontend/src/app/pipes/translation.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import { of } from "rxjs";
 import { map } from "rxjs/operators";
+import { AvailableLanguages } from "../enums";
 import { TranslationService } from "../services";
 
 @Pipe({ name: "translate" })
 export class TranslationPipe implements PipeTransform {
   constructor(private translationService: TranslationService) {}
 
-  transform(text: string, target: string) {
+  transform(text: string, target: AvailableLanguages) {
+    if (target === AvailableLanguages.EN) {
+      return of(text);
+    }
     return this.translationService.translate(text, target).pipe(
       map((x) => {
         const [{ translatedText }] = x.data.translations;
